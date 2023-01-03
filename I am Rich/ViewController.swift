d7c4a6943c0c05d6e5eeac9b83a488c6d87f0758
Xinran <zhxyn6@gmail.com>
2023-01-03 16:14:46 -0500

    display a diamond

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Yujun Zhao on 1/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
